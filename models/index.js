cc22ac945e807ecedb78325afe8622ea6dc63f5b
vedelopment <vedelopment@gmail.com>
2017-03-01 10:42:27 -0800

    add heroku connection to mongo

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/chaperon");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/chaperon" );
 
 var User = require('./user');
 
